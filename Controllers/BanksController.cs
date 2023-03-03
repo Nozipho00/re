d0d3c6ca8ba8066cacf458e0eb3710f202c4fdbe
Nozipho00 <102392855+Nozipho00@users.noreply.github.com>
2023-03-03 09:48:46 +0200

    begin

---
+++ Controllers/BanksController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OM_dummy1.Models;
+
+namespace OM_dummy1.Controllers
+{
+    public class BanksController : Controller
+    {
+        private OMdummyEntities db = new OMdummyEntities();
+
+        // GET: Banks
+        public ActionResult Index()
+        {
+            var banks = db.Banks.Include(b => b.Customer).Include(b => b.Policy);
+            return View(banks.ToList());
+        }
+
+        // GET: Banks/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bank bank = db.Banks.Find(id);
+            if (bank == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bank);
+        }
+
+        // GET: Banks/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName");
+            ViewBag.PolicyID = new SelectList(db.Policies, "PolicyID", "PolicyName");
+            return View();
+        }
+
+        // POST: Banks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BranchCode,BankName,BankType,CustomerID,PolicyID")] Bank bank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Banks.Add(bank);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName", bank.CustomerID);
+            ViewBag.PolicyID = new SelectList(db.Policies, "PolicyID", "PolicyName", bank.PolicyID);
+            return View(bank);
+        }
+
+        // GET: Banks/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bank bank = db.Banks.Find(id);
+            if (bank == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName", bank.CustomerID);
+            ViewBag.PolicyID = new SelectList(db.Policies, "PolicyID", "PolicyName", bank.PolicyID);
+            return View(bank);
+        }
+
+        // POST: Banks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BranchCode,BankName,BankType,CustomerID,PolicyID")] Bank bank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bank).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName", bank.CustomerID);
+            ViewBag.PolicyID = new SelectList(db.Policies, "PolicyID", "PolicyName", bank.PolicyID);
+            return View(bank);
+        }
+
+        // GET: Banks/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bank bank = db.Banks.Find(id);
+            if (bank == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bank);
+        }
+
+        // POST: Banks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Bank bank = db.Banks.Find(id);
+            db.Banks.Remove(bank);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/QueriesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OM_dummy1.Models;
+
+namespace OM_dummy1.Controllers
+{
+    public class QueriesController : Controller
+    {
+        private OMdummyEntities db = new OMdummyEntities();
+
+        // GET: Queries
+        public ActionResult Index()
+        {
+            var queries = db.Queries.Include(q => q.Agent).Include(q => q.Customer);
+            return View(queries.ToList());
+        }
+
+        // GET: Queries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Query query = db.Queries.Find(id);
+            if (query == null)
+            {
+                return HttpNotFound();
+            }
+            return View(query);
+        }
+
+        // GET: Queries/Create
+        public ActionResult Create()
+        {
+            ViewBag.AgentID = new SelectList(db.Agents, "AgentID", "AgentName");
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName");
+            return View();
+        }
+
+        // POST: Queries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "QueryID,QueryDescription,QueryType,CustomerID,AgentID")] Query query)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Queries.Add(query);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AgentID = new SelectList(db.Agents, "AgentID", "AgentName", query.AgentID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName", query.CustomerID);
+            return View(query);
+        }
+
+        // GET: Queries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Query query = db.Queries.Find(id);
+            if (query == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AgentID = new SelectList(db.Agents, "AgentID", "AgentName", query.AgentID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName", query.CustomerID);
+            return View(query);
+        }
+
+        // POST: Queries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "QueryID,QueryDescription,QueryType,CustomerID,AgentID")] Query query)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(query).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AgentID = new SelectList(db.Agents, "AgentID", "AgentName", query.AgentID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName", query.CustomerID);
+            return View(query);
+        }
+
+        // GET: Queries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Query query = db.Queries.Find(id);
+            if (query == null)
+            {
+                return HttpNotFound();
+            }
+            return View(query);
+        }
+
+        // POST: Queries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Query query = db.Queries.Find(id);
+            db.Queries.Remove(query);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
